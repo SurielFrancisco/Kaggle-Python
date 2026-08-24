0ca068b9d1dd70b4f2a2f909924334026e575dc1
Suriel Francisco <surielfrancisco@MacBook-Pro-de-Suriel.local>
2026-08-24 01:15:19 -0600

    Functions_3

--- FunctionsGettingHelp/Example_1.py
+++ FunctionsGettingHelp/Example_1.py
@@ -1,2 +1,11 @@
-# Put your test code here
-round_to_two_places(2)
+def to_smash(total_candies, n_friends=3):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    return total_candies % n_friends
+
+# Check your answer
+q3.check()
