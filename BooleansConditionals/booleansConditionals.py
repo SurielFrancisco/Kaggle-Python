3d3163c15f5f5e66da0b0ee9e52f41fc73daff13
Suriel Francisco <surielfrancisco@MacBook-Pro-de-Suriel.local>
2026-08-24 01:24:23 -0600

    booleans_1

---
+++ BooleansConditionals/booleansConditionals.py
@@ -0,0 +1,11 @@
+# Your code goes here. Define a function called 'sign'
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+# Check your answer
+q1.check()
