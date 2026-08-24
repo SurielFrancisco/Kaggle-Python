94545a78e4a47620823a034c715d0d5dd5538016
Suriel Francisco <surielfrancisco@MacBook-Pro-de-Suriel.local>
2026-08-24 01:14:06 -0600

    Functions_2

--- FunctionsGettingHelp/Example_1.py
+++ FunctionsGettingHelp/Example_1.py
@@ -1,13 +1,2 @@
-def round_to_two_places(num):
-    """Return the given number rounded to two decimal places. 
-    
-    >>> round_to_two_places(3.14159)
-    3.14
-    """
-    # Replace this body with your own code.
-    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
-    # because after we begin a code block, Python requires at least one line of code)
-    return round(num, 2)
-
-# Check your answer
-q1.check()
+# Put your test code here
+round_to_two_places(2)
